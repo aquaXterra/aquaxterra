31f1ba9cbc675de525ddb4de7567b14548c62bfd
Quentin Read <quentin.read@gmail.com>
2016-11-02 11:52:26 -0400

    Add bbs and huc data, plot histograms
    Data extracted from the HPCC (see readshps.r), put on the repo, and some
    summary histograms plotted (see howmanyhucs.r)

---
+++ code/howmanyhucs.r
@@ -0,0 +1,49 @@
+# Quantify the number of bbs routes in each polygon
+# author: QDR
+# Project: Aquaxterra
+# Created: 02 Nov 2016
+
+bbs_huc <- read.csv('data/BBS_SpatialJoin_Final.csv', stringsAsFactors = FALSE)
+
+ns <- strsplit(bbs_huc$rtestopNo, '-')
+bbs_huc$route <- sapply(ns, '[', 1)
+bbs_huc$stopno <- sapply(ns, '[', 2)
+
+library(dplyr)
+
+huc4n <- bbs_huc %>% group_by(HUC4) %>% summarize(nroutes = length(unique(route)), nstops = length(rtestopNo))
+huc8n <- bbs_huc %>% group_by(HUC8) %>% summarize(nroutes = length(unique(route)), nstops = length(rtestopNo))
+huc12n <- bbs_huc %>% group_by(HUC12) %>% summarize(nroutes = length(unique(route)), nstops = length(rtestopNo))
+
+# How many routes are in multiple polygons of each level?
+hucsperroute <- bbs_huc %>% group_by(route) %>% summarize(n_huc4 = length(unique(HUC4)), n_huc8 = length(unique(HUC8)), n_huc12 = length(unique(HUC12)))
+
+table(hucsperroute$n_huc4)
+table(hucsperroute$n_huc8)
+table(hucsperroute$n_huc12)
+
+# Load huc area
+huc12area <- read.csv('data/huc12out.csv', stringsAsFactors = FALSE)
+bbs_huc <- left_join(bbs_huc, huc12area)
+
+huc4area <- read.csv('data/huc4area.csv')
+huc8area <- read.csv('data/huc8area.csv')
+
+library(ggplot2)
+source('~/qutil.r')
+
+pdf('figs/bbs_huc_histograms.pdf', height=6, width=6)
+
+ggplot(huc4n, aes(x = nroutes)) + geom_bar(stat='count') + theme_bw() + scale_y_continuous(expand = c(0,0), limits = c(0,15)) + ggtitle('Number of BBS routes in each HUC4')
+ggplot(huc8n, aes(x = nroutes)) + geom_bar(stat='count') + theme_bw() + scale_y_continuous(expand = c(0,0), limits = c(0,550)) + ggtitle('Number of BBS routes in each HUC8')
+ggplot(huc12n, aes(x = nroutes)) + geom_bar(stat='count') + theme_bw() + scale_y_continuous(expand = c(0,0), limits = c(0,15000)) + ggtitle('Number of BBS routes in each HUC12')
+
+ggplot(hucsperroute, aes(x = n_huc4)) + geom_bar(stat='count') + theme_bw() + scale_y_continuous(expand = c(0,0), limits = c(0,2500)) + ggtitle('Number of HUC4s that each BBS route crosses')
+ggplot(hucsperroute, aes(x = n_huc8)) + geom_bar(stat='count') + theme_bw() + scale_y_continuous(expand = c(0,0), limits = c(0,1600)) + ggtitle('Number of HUC8s that each BBS route crosses')
+ggplot(hucsperroute, aes(x = n_huc12)) + geom_bar(stat='count') + theme_bw() + scale_y_continuous(expand = c(0,0), limits = c(0,860)) + scale_x_continuous(breaks=0:12) + ggtitle('Number of HUC12s that each BBS route crosses')
+
+ggplot(huc4area, aes(x = AreaSqKm)) + geom_histogram(bins = 20) + theme_bw() + scale_y_continuous(expand = c(0,0), limits = c(0,35)) + ggtitle('HUC4 sizes')
+ggplot(huc8area, aes(x = AreaSqKm)) + geom_histogram(bins = 20) + theme_bw() + scale_y_continuous(expand = c(0,0), limits = c(0,600)) + scale_x_continuous(limits=c(0,20000)) + qSubtitle('HUC8 sizes', 'eight very large outliers not plotted')
+ggplot(huc12area, aes(x = AreaSqKm)) + geom_histogram(bins = 20) + theme_bw() + scale_y_continuous(expand = c(0,0), limits = c(0,200)) + scale_x_continuous(limits=c(0,5000)) + qSubtitle('HUC12 sizes', 'thirty-two very large outliers not plotted')
+
+dev.off()
--- code/readshps.r
+++ code/readshps.r
@@ -1,6 +1,43 @@
+# Read SHPs and get info on area
+# Author: QDR
+# Project: Aquaxterra
+# Last modified: 02 Nov 2016
+
 library(rgdal)
 setwd('/mnt/research/aquaxterra/DATA/reprojected_data')
 
 huc12 <- readOGR(dsn = 'BBS_SpatialJoin', layer = 'HUC12')
 bbsbuffers <- readOGR(dsn = 'BBS_SpatialJoin', layer = 'bbs_buffers_Albers')
 iws <- readOGR(dsn = 'BBS_SpatialJoin', layer= 'IWS')
+
+# Calculate area of the polygons in huc12
+
+library(raster)
+crs(huc12)
+hucarea <- area(huc12)
+# Output in a simple format
+hucarea <- data.frame(Name = huc12$Name, HUC12 = huc12$HUC12, area = hucarea)
+
+# Turns out it already contains an area. Just output it.
+
+hucout <- huc12@data[, c('AreaAcres','AreaSqKm','States','HUC12','Name')]
+
+# Extract the huc4 and huc8 info from the huc12 column
+
+hucnames <- as.character(huc12@data$HUC12)
+huc4names <- as.numeric(substr(hucnames, 1, 4))
+huc8names <- as.numeric(substr(hucnames, 1, 8))
+
+hucout$HUC4 <- huc4names
+hucout$HUC8 <- huc8names
+
+# Sum the areas of each huc
+
+library(dplyr)
+huc4area <- hucout %>% group_by(HUC4) %>% summarize(AreaSqKm = sum(AreaSqKm))
+huc8area <- hucout %>% group_by(HUC8) %>% summarize(AreaSqKm = sum(AreaSqKm))
+
+
+write.csv(hucout, file = '~/data/aquaxterra/huc12out.csv', row.names = FALSE)
+write.csv(huc4area, file = '~/data/aquaxterra/huc4area.csv', row.names = FALSE)
+write.csv(huc8area, file = '~/data/aquaxterra/huc8area.csv', row.names = FALSE)
