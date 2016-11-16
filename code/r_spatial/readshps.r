ef282193350e03742c3c5181ce7d10e273d198f4
Quentin Read <quentin.read@gmail.com>
2016-11-16 13:43:04 -0500

    Attempt to get rid of lakes in HUC12
    I got rid of all HUC12 areas typed as 'W' for Water, and also as many of
    the ones typed 'C' for Closed basin that had names indicating that they
    are lakes. This gets rid of about 1000 of the >80000 HUC12s.

--- code/r_spatial/readshps.r
+++ code/r_spatial/readshps.r
@@ -1,7 +1,7 @@
 # Read SHPs and get info on area
 # Author: QDR
 # Project: Aquaxterra
-# Last modified: 02 Nov 2016
+# Last modified: 16 Nov 2016
 
 library(rgdal)
 setwd('/mnt/research/aquaxterra/DATA/reprojected_data')
@@ -41,3 +41,18 @@ huc8area <- hucout %>% group_by(HUC8) %>% summarize(AreaSqKm = sum(AreaSqKm))
 write.csv(hucout, file = '~/data/aquaxterra/huc12out.csv', row.names = FALSE)
 write.csv(huc4area, file = '~/data/aquaxterra/huc4area.csv', row.names = FALSE)
 write.csv(huc8area, file = '~/data/aquaxterra/huc8area.csv', row.names = FALSE)
+
+# Added 16 Nov
+# Try to get rid of some of the big lakes.
+huc12big <- subset(huc12, AreaSqKm > 2500)
+
+# HUType already notes which of the HUCs are water. Metadata is found here: https://pubs.usgs.gov/tm/11/a3/pdf/tm11-a3.pdf
+huc12water <- subset(huc12, HUType == 'W') # Water, includes lakes and bays. All should be removed.
+huc12closedbasin <- subset(huc12, HUType == 'C') # Closed basin, includes areas of the Great Basin but also includes Great Salt Lake. Not all should be removed.
+huc12namedlakes <- subset(huc12, grepl('Lake|lake', huc12@data$Name)) # Doesn't work because many of these are land areas with Lake in the name, such as frontage of lakes
+huc12endorheiclakes <- subset(huc12, grepl('Lake|lake', huc12@data$Name) & HUType=='C') # Most of these are lakes, including things like Great Salt Lake and Crater Lake. 
+# It looks like all the lake ones that also contain land area have a hyphen in the name, so we can get rid of all the ones with hyphens
+
+huc12_toremove <- huc12@data$HUType == 'W' | (grepl('Lake|lake', huc12@data$Name) & huc12@data$HUType=='C' & !grepl('-', huc12@data$Name))
+
+huc12nolakes <- subset(huc12, !huc12_toremove)
