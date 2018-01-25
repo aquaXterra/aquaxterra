5066efdb64510aeb56de7b0524a9a360f9842069
Quentin Read <quentin.read@gmail.com>
2018-01-25 15:34:12 -0500

    add error catching function to huc4 river calc

--- code/r_spatial/nhd_huc4.r
+++ code/r_spatial/nhd_huc4.r
@@ -18,41 +18,47 @@ for (i in 1:length(alldirs)) {
   
   fp_huc <- file.path(fp, alldirs[i], 'Shape')
   
-  # Load NHD shapefiles
-  
-  lake_nhd <- readOGR(dsn = fp_huc, layer = 'NHDWaterbody')
-  lake_lookup <- read.csv('/mnt/research/aquaxterra/CODE/R/nhd/nhd_waterbody_lookup.csv', stringsAsFactors = FALSE)
-  
-  river_nhd <- readOGR(dsn = fp_huc, layer = 'NHDFlowline')
-  river_lookup <- read.csv('/mnt/research/aquaxterra/CODE/R/nhd/nhd_flowline_lookup.csv', stringsAsFactors = FALSE)
-  
-  # Project to equal area so we can calculate length or area
-  aea_crs <- '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +towgs84=0,0,0'
-  lake_nhd_aea <- spTransform(lake_nhd, CRSobj = aea_crs)
-  river_nhd_aea <- spTransform(river_nhd, CRSobj = aea_crs)
-  
-  lake_area <- gArea(lake_nhd_aea, byid = TRUE)
-  river_length <- gLength(river_nhd_aea, byid = TRUE)
-  
-  lake_data <- lake_nhd@data %>%
-    rename(FCode = FCODE) %>%
-    left_join(lake_lookup) %>%
-    cbind(area = as.numeric(lake_area))
-  
-  lake_summary[[i]] <- lake_data %>%
-    filter(include == 'yes') %>%
-    group_by(category, subcategory, permanence) %>%
-    summarize(area = sum(area))
-  
-  river_data <- river_nhd@data %>%
-    rename(FCode = FCODE) %>%
-    left_join(river_lookup) %>%
-    cbind(length = as.numeric(river_length))
-  
-  river_summary[[i]] <- river_data %>%
-    filter(include == 'yes') %>%
-    group_by(category, subcategory, permanence) %>%
-    summarize(length = sum(length))
+  if (!file.exists(fp_huc)) {
+    lake_summary[[i]] <- data.frame(category = NA, subcategory = NA, permanence = NA, area = NA)
+    river_summary[[i]] <- data.frame(category = NA, subcategory = NA, permanence = NA, length = NA)
+  } else {
+    
+    # Load NHD shapefiles
+    
+    lake_nhd <- readOGR(dsn = fp_huc, layer = 'NHDWaterbody')
+    lake_lookup <- read.csv('/mnt/research/aquaxterra/CODE/R/nhd/nhd_waterbody_lookup.csv', stringsAsFactors = FALSE)
+    
+    river_nhd <- readOGR(dsn = fp_huc, layer = 'NHDFlowline')
+    river_lookup <- read.csv('/mnt/research/aquaxterra/CODE/R/nhd/nhd_flowline_lookup.csv', stringsAsFactors = FALSE)
+    
+    # Project to equal area so we can calculate length or area
+    aea_crs <- '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +towgs84=0,0,0'
+    lake_nhd_aea <- spTransform(lake_nhd, CRSobj = aea_crs)
+    river_nhd_aea <- spTransform(river_nhd, CRSobj = aea_crs)
+    
+    lake_area <- gArea(lake_nhd_aea, byid = TRUE)
+    river_length <- gLength(river_nhd_aea, byid = TRUE)
+    
+    lake_data <- lake_nhd@data %>%
+      rename(FCode = FCODE) %>%
+      left_join(lake_lookup) %>%
+      cbind(area = as.numeric(lake_area))
+    
+    lake_summary[[i]] <- lake_data %>%
+      filter(include == 'yes') %>%
+      group_by(category, subcategory, permanence) %>%
+      summarize(area = sum(area))
+    
+    river_data <- river_nhd@data %>%
+      rename(FCode = FCODE) %>%
+      left_join(river_lookup) %>%
+      cbind(length = as.numeric(river_length))
+    
+    river_summary[[i]] <- river_data %>%
+      filter(include == 'yes') %>%
+      group_by(category, subcategory, permanence) %>%
+      summarize(length = sum(length))
+  }
 }
 
 close(pb)
